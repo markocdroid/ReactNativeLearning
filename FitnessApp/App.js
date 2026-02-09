8e734092ffd95663139f029279c7064e500e353e
markocdroid <markoc.dev@gmail.com>
2026-02-09 15:42:52 -0500

    Finished the lab exercise

--- FitnessApp/App.js
+++ FitnessApp/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import FitnessWorld from './FitnessWorld'
+import { Platform, StyleSheet } from 'react-native';
+import FitnessWorld from './FitnessWorld';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <FitnessWorld />
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <FitnessWorld />
+    </SafeAreaProvider>
+  );
+}
+
+let _paddingBottom = 0;
+
+if (Platform.OS === 'android') {
+  _paddingBottom = 50;
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'black',
-    },
+  container: {
+    flex: 1,
+    paddingTop: 25, //Clearing notch
+    paddingBottom: _paddingBottom, //clearing nav for Android.
+    flexDirection: 'vertical',
+    backgroundColor: 'black',
+  },
 });
